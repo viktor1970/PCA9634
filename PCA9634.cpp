1d2763b5d7c7db04da9bb6ff9f3d0c493f178a0d
Vittorio <vittorio@vittorio.no.it>
2019-04-10 20:44:25 +0200

    Add files via upload

--- PCA9634.cpp
+++ PCA9634.cpp
@@ -9,51 +9,46 @@ PCA9634::PCA9634(uint8_t addr){
 
 void PCA9634::begin(uint8_t mode1, uint8_t mode2){
     // all leds default OFF; PWM set to 0
-    write_reg(PCA9634_REG_MODE1,0);
-    write_reg(PCA9634_REG_MODE2,mode2); 
+    write_reg(PCA9634_MODE1,0);
+    write_reg(PCA9634_MODE2,mode2); 
 	_reg0C=0;
 	_reg0D=0;
+	_reg0Cg=0;
+	_reg0Dg=0;
 	_mode1=mode1;
 	_mode2=mode2;
 }
 
 uint8_t PCA9634::set_mode1(uint8_t mode1){
-    return write_reg(PCA9634_REG_MODE1,mode1);
+    return write_reg(PCA9634_MODE1,mode1);
 }
 
 uint8_t PCA9634::set_mode2(uint8_t mode2){
-    return write_reg (PCA9634_REG_MODE2,mode2);
+    return write_reg (PCA9634_MODE2,mode2);
 }
 
 uint8_t PCA9634::set_pwm(uint8_t led, uint8_t duty_cycle){
     if (led>7) return -1;
-	return write_reg(PCA9634_REG_PWM0+led,duty_cycle);
+	return write_reg(PCA9634_PWM+led,duty_cycle);
 }
 
 uint8_t PCA9634::set_global_pwm(uint8_t duty_cycle){
-    return write_reg(PCA9634_REG_GRPPWM, duty_cycle );
+    return write_reg(PCA9634_GRPPWM, duty_cycle );
 }
 
 uint8_t PCA9634::set_blink_freq(uint8_t frequency){
-    return write_reg(PCA9634_REG_GRPFREQ, frequency);
+    return write_reg(PCA9634_GRPFREQ, frequency);
 }
 
-uint8_t PCA9634::getReg(){
-    return _reg0C;
+void PCA9634::swReset(void){
+    for(uint8_t n=0; n<8; n++) write_reg(PCA9634_PWM+n,0);
+	write_reg(PCA9634_GRPPWM,0);
+    write_reg(PCA9634_GRPFREQ,0);
+    write_reg(PCA9634_MODE1, _mode1);
+    write_reg(PCA9634_MODE2, _mode2); 
 }
 
-void PCA9634::swReset(){
-    for(uint8_t n=0; n<8; n++) write_reg(PCA9634_REG_PWM0+n,0);
-	write_reg(PCA9634_REG_GRPPWM,0);
-    write_reg(PCA9634_REG_GRPFREQ,0);
-    write_reg( PCA9634_REG_MODE1, _mode1);
-    write_reg( PCA9634_REG_MODE2, _mode2); 
-
-}
-
-
-uint8_t PCA9634::led_mode(uint8_t led, uint8_t mode)
-{
+uint8_t PCA9634::led_mode(uint8_t led, uint8_t mode){
  if(led>7) return 255;
  switch(led)
  {
@@ -72,10 +67,31 @@ uint8_t PCA9634::led_mode(uint8_t led, uint8_t mode)
   { _reg0D|=mode<<(led*2); write_reg(0x0D, _reg0D);}
 }
 
+uint8_t PCA9634::set_led_global(uint8_t led, uint8_t mode){
+ if(led>7) return 255;
+ switch(led)
+ {
+  case 0: {_reg0Cg &=0xFC; break;}
+  case 1: {_reg0Cg &=0xF3; break;}
+  case 2: {_reg0Cg &=0xCF; break;}
+  case 3: {_reg0Cg &=0x3F; break;}
+  case 4: {_reg0Dg &=0xFC; break;}
+  case 5: {_reg0Dg &=0xF3; break;}
+  case 6: {_reg0Dg &=0xCF; break;}
+  case 7: {_reg0Dg &=0x3F; break;}
+ }
+ if(led<4){ _reg0Cg|=mode<<(led*2); }
+  else    { _reg0Dg|=mode<<(led*2); }
+}
+
+uint8_t PCA9634::apply_led_global(void){
+ write_reg(0x0C, _reg0Cg); _reg0C=_reg0Cg;
+ write_reg(0x0D, _reg0Dg); _reg0D=_reg0Dg;
+}
+
 uint8_t PCA9634::write_reg(uint8_t reg, uint8_t value){
     Wire.beginTransmission( _addr );
     Wire.write( reg );
     Wire.write( value );
     return Wire.endTransmission( );
-
 }
--- PCA9634.h
+++ PCA9634.h
@@ -6,21 +6,13 @@
 #ifndef __pca9634__
 #define __pca9634__
 
-
-#define PCA9634_REG_MODE1		0x00		// Mode register 1
-#define PCA9634_REG_MODE2		0x01		// Mode register 2
-#define PCA9634_REG_PWM0		0x02		// brightness control LED0
-#define PCA9634_REG_PWM1		0x03		// brightness control LED1
-#define PCA9634_REG_PWM2		0x04		// brightness control LED2
-#define PCA9634_REG_PWM3		0x05		// brightness control LED3
-#define PCA9634_REG_PWM4		0x06		// brightness control LED4
-#define PCA9634_REG_PWM5		0x07		// brightness control LED5
-#define PCA9634_REG_PWM6		0x08		// brightness control LED6
-#define PCA9634_REG_PWM7		0x09		// brightness control LED7
-#define PCA9634_REG_GRPPWM		0x0A		// group duty cycle
-#define PCA9634_REG_GRPFREQ		0x0B		// group frequency
-#define PCA9634_REG_LEDOUT0		0x0C		// LED output state 0
-#define PCA9634_REG_LEDOUT1		0x0D		// LED output state 1
+#define PCA9634_MODE1		0x00		// Mode register 1
+#define PCA9634_MODE2		0x01		// Mode register 2
+#define PCA9634_PWM			0x02		// brightness control LED0 (LED1=0X03, LED2=0x04 and so on)
+#define PCA9634_GRPPWM		0x0A		// group duty cycle
+#define PCA9634_GRPFREQ		0x0B		// group frequency
+#define PCA9634_LEDOUT0		0x0C		// LED output state 0
+#define PCA9634_LEDOUT1		0x0D		// LED output state 1
 
 // LEDOUT0 and LEDOUT1 output state register
 #define PCA9634_LEDOFF			0x00		// LED OFF 
@@ -29,7 +21,7 @@
 #define PCA9634_LEDBLNK			0x03		// LED PWM GROUP
 
 // MODE register 2 (MODE register 1 not yet implemented)
-#define PCA9634_DMBLNK 			0x20		// Group contro: blinking (default: dimming)
+#define PCA9634_DMBLNK 			0x20		// Group control: blinking (default: dimming)
 #define PCA9634_INVRT  			0x10		// Output logic state inverted (default: not inverted)
 #define PCA9634_OUTDRV 			0x00		// Open-drain output (default: totem-pole output)
 
@@ -42,30 +34,42 @@ class PCA9634
 {
     public:
         
-        // i2c chip address
+        // addr: I2C address
         PCA9634(uint8_t addr);
 
-        // sets up pca9634 and turns all leds off
+        // Set mode1 (0x00, will be implemented in future)and mode2
         void begin(uint8_t mode1, uint8_t mode2);
 
-        // these 3 functions do writes directly to pca9634 registers
-        // read that manual if you want to use them.
-        uint8_t set_mode2(uint8_t mode2);
+		//Set mode 1
         uint8_t set_mode1(uint8_t mode1);
+		//Set mode 1
+        uint8_t set_mode2(uint8_t mode2);
+		//Set individual pwm duty-cycle
         uint8_t set_pwm(uint8_t led, uint8_t duty_cycle);
-        uint8_t set_global_pwm(uint8_t duty_cycle);
-        uint8_t set_blink_freq(uint8_t frequency);
-		void swReset();
 		
-		uint8_t getReg();
+		//Set global pwm duty-cycle (used when DMBLNK mode is set)
+        uint8_t set_global_pwm(uint8_t duty_cycle);
+		//Set global blink frequency (used when DMBLNK mode is set)
+        uint8_t set_blink_freq(uint8_t frequency);
+
+		//Reset all pwm/freq to 0; set mode 1 and mode 2 as begin
+		void swReset(void);
 		
 		//set individual led mode
 		uint8_t led_mode(uint8_t led, uint8_t mode);
 
+        //set led mode WITHOUT apply it
+		uint8_t set_led_global(uint8_t led, uint8_t mode);
+
+		//Set led mode register with values defined in set_led_global
+		uint8_t apply_led_global(void);
+		
     private:
         uint8_t _addr;
 		uint8_t _reg0C;
 		uint8_t _reg0D;
+		uint8_t _reg0Cg;
+		uint8_t _reg0Dg;
 		uint8_t _mode1;
 		uint8_t _mode2;
         uint8_t write_reg(uint8_t reg, uint8_t value);     
